be8e5b1eef17614a9105be8665b87ac285e29559
Joe Van Boxtel <joe@pndlm.com>
2019-11-26 21:03:52 -0800

    Make the CountMeta initializer public.

--- Sources/JSONAPISpec/CountMeta.swift
+++ Sources/JSONAPISpec/CountMeta.swift
@@ -8,6 +8,11 @@ public struct CountMeta: Codable {
     public var totalCount: Int
     public var count: Int
     
+    public init(totalCount: Int, count: Int) {
+        self.totalCount = totalCount
+        self.count = count
+    }
+    
     enum CodingKeys: String, CodingKey {
         case totalCount = "total_count"
         case count
